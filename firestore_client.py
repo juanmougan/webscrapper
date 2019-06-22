b5182f7ba9309f0daf7014343e8384e9b81e635f
Juan Manuel Mougan <juanmougan@gmail.com>
2019-06-21 21:28:54 -0300

    Generified client

--- firestore_client.py
+++ firestore_client.py
@@ -1,29 +1,22 @@
 import firebase_admin
+import jsons
 from firebase_admin import credentials
 from firebase_admin import firestore
 
 class FirestoreClient:
-	CREDS = './config/fcm_service_account.json'
-	COLLECTION = u'articles'
-
-	def __init__(self):
-		cred = credentials.Certificate(CREDS)
+	def __init__(self, creds, collection):
+		cred = credentials.Certificate(creds)
 		firebase_admin.initialize_app(cred)
+		self.collection = collection
 		self.db = firestore.client()
 
-	def store_in_collection(document, article):
-		doc_ref = db.collection(COLLECTION).document(article.link)		# TODO maybe add a UUID
-		doc_ref.set({
-		    u'link': article.link,
-		    u'address': article.address,
-		    u'rent_price': article.prices.rent,
-		    u'expenses_price': article.expenses.rent
-		})
+	def store_in_collection(self, article):
+		self.db.collection(self.collection).add(jsons.dump(article))
 
-	def fetch_all_from_collections():
-		doc_ref = db.collection(COLLECTION)
+	def fetch_all_from_collection(self):
+		documents = []
+		doc_ref = self.db.collection(self.collection)
 		docs = doc_ref.get()
-
-		# TODO add to a collection, instead of printing
 		for doc in docs:
-		    print(u'{} => {}'.format(doc.id, doc.to_dict()))
+		    documents.append(doc.to_dict())
+		return documents
